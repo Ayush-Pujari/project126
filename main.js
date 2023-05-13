c6c3ea308d6f39673caa4637c98fab0d86ba74fd
Ayush <88432311+Ayush-Pujari@users.noreply.github.com>
2023-05-13 13:41:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,26 @@ function setup(){
 
     video=createCapture(VIDEO);
     video.hide();
+
+    poseNet=ml5.poseNet(video,modalLoaded);
+    poseNet.on('pose',gotPoses);
 }
 function draw(){
     image(video,0,0,600,500);
 }
+function modalLoaded(){
+    console.log("Posenet is inittialized");
+  }
+  function gotPoses(result){
+      if(result.length > 0){
+          console.log(result);
+          leftWristX=result[0].pose.leftWrist.x;
+          leftWristY=result[0].pose.leftWrist.y;
+          rightWristX=result[0].pose.rightWrist.x;
+          rightWristY=result[0].pose.rightWrist.y;
+          console.log(" leftWristX = "+leftWristX+ "leftwristY = "+leftWristY);
+          console.log("rightWristX = "+ rightWristX+"rightWristY = "+ rightWristY);
+      }
+  
+      
+  }
